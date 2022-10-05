c06394409fa7d1c2f5346f95a5123274f2fe77eb
Vilde <69961376+vildekure@users.noreply.github.com>
2022-10-05 16:37:13 +0200

    alle elementer i skjermbilde lagt til, starten på main virker ok

--- app/src/main/java/com/example/mappe2avtaler/MainActivity.java
+++ app/src/main/java/com/example/mappe2avtaler/MainActivity.java
@@ -20,7 +20,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        navninn = (EditText) findViewById(R.id.navn);
+        telefoninn = (EditText) findViewById(R.id.tlf);
+        idinn = (EditText) findViewById(R.id.min_id);
+        utskrift = (TextView) findViewById(R.id.utskrift);
+        dbHelper = new DBHandler(this);
+        db = dbHelper.getWritableDatabase();
 
     }
 
+    public void leggtil(View v) {
+    }
+
+    public void visalle(View v) {
+    }
+
+    public void slett(View v) {
+    }
+
+    public void oppdater(View v) {
+    }
 }
