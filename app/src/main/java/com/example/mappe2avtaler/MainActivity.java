00987e6ca8514e3f3d0b6fcd4948b93b1249f26a
Vilde <69961376+vildekure@users.noreply.github.com>
2022-10-05 16:30:15 +0200

    startet på Database (Må ta en ny titt på DBHandler og MainAk

--- app/src/main/java/com/example/mappe2avtaler/MainActivity.java
+++ app/src/main/java/com/example/mappe2avtaler/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.mappe2avtaler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    EditText navninn, telefoninn, idinn;
+    TextView utskrift;
+    DBHandler dbHelper;
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
 }
